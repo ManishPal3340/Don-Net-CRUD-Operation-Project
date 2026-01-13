50d28ba1d0073284c17e18a6d6c089536f583b0c
Manish Pal <manishpal3340@gmail.com>
2026-01-13 15:56:02 +0530

    Add files via upload

---
+++ insert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Data.SqlClient; // Ya System.Data.SqlClient
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Connection string mein apna sahi server aur DB name dalein
+        string cs = @"Data Source=LAPTOP-3OQHG7D2\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;TrustServerCertificate=True";
+
+        try
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                // Query mein table name check karlein (Student hai ya kuch aur)
+                string query = "INSERT INTO Student (Id, Name, Age) VALUES (1, 'Manish', 22)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                Console.WriteLine("Data Inserted Successfully!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Kuch error aaya hai: " + ex.Message);
+        }
+    }
+}
---
+++ read.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string cs = @"Data Source=LAPTOP-3OQHG7D2\SQLEXPRESS;Initial Catalog=MyDB;Integrated Security=True;TrustServerCertificate=True";
+
+        SqlConnection con = new SqlConnection(cs);
+
+        string query = "SELECT * FROM Student";
+        SqlCommand cmd = new SqlCommand(query, con);
+
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+
+        while (dr.Read())
+        {
+            Console.WriteLine(dr["Id"] + " " + dr["Name"] + " " + dr["Age"]);
+        }
+
+        con.Close();
+    }
+}
